8ba10ff86dbbe1e0eaecb68c30fcbca8f1f62850
CiaranCheetham <ciarancheetham@hotmail.co.uk>
2020-06-09 21:13:45 +0100

    Added deliberately failing unit test

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Ciaran' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_browser_title_correct(self):
+
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('Ciaran', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
